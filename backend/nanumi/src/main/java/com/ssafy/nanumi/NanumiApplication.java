2e15b876eacfcc062e4c757317d8014fbfba7408
김우진 <k0109974@naver.com>
2023-04-14 13:27:59 +0900

    first

---
+++ backend/nanumi/src/main/java/com/ssafy/nanumi/NanumiApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.nanumi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NanumiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NanumiApplication.class, args);
+	}
+
+}
