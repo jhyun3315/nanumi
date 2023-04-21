a07bb5d7f6d387fd4c58ebdca137ff9339a8e620
한상준 <hsjun1996@naver.com>
2023-04-21 17:56:38 +0900

    Feat : View map

--- frontend/nanumiMobile/components/web/Map.jsx
+++ frontend/nanumiMobile/components/web/Map.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import {Dimensions, StyleSheet} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Dimensions, StyleSheet, Alert, PermissionsAndroid} from 'react-native';
 import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
+import Geolocation from 'react-native-geolocation-service';
 
 const {width, height} = Dimensions.get('window');
 const INITIAL_COORINATE = {
@@ -8,19 +9,48 @@ const INITIAL_COORINATE = {
   longitude: -122.4324,
 };
 
-const Map = ({coordinate = INITIAL_COORINATE}) => {
+const Map = () => {
+  const [coordinate, setCoordinate] = useState({});
+
+  const requestPermissions = async () => {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+    );
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+      Geolocation.getCurrentPosition(
+        position => {
+          console.log(position.coords);
+          setCoordinate({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          });
+        },
+        error => {
+          Alert.alert('위치정보를 얻어오는데 실패했습니다.');
+          console.log(error);
+        },
+        {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
+      );
+    }
+  };
+
+  useEffect(() => {
+    requestPermissions();
+  }, []);
+
   return (
-    <MapView
-      provider={PROVIDER_GOOGLE}
-      style={styles.mapView}
-      region={{
-        latitude: coordinate.latitude,
-        longitude: coordinate.longitude,
-        latitudeDelta: 0.004,
-        longitudeDelta: 0.004,
-      }}>
-      <Marker key={1} coordinate={coordinate} />
-    </MapView>
+    <>
+      {coordinate.latitude && coordinate.longitude && (
+        <MapView
+          provider={PROVIDER_GOOGLE}
+          style={styles.mapView}
+          region={coordinate}>
+          <Marker key={1} coordinate={coordinate} />
+        </MapView>
+      )}
+    </>
   );
 };
 
