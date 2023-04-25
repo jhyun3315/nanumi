d8b726d8389ac8811d79b9fd17c15a5434c6cf04
한상준 <hsjun1996@naver.com>
2023-04-25 10:09:27 +0900

    Fix : Remove react-native-paper-provider

--- frontend/nanumiMobile/App.jsx
+++ frontend/nanumiMobile/App.jsx
@@ -2,15 +2,12 @@ import React from 'react';
 import StackNavigator from './navigator/StackNavigator';
 import {StatusBar} from 'react-native';
 import {RecoilRoot} from 'recoil';
-import {Provider as PaperProvider} from 'react-native-paper';
 
 const App = () => {
   return (
     <RecoilRoot>
-      <PaperProvider>
-        <StatusBar />
-        <StackNavigator />
-      </PaperProvider>
+      <StatusBar />
+      <StackNavigator />
     </RecoilRoot>
   );
 };
