f8093805c9949c0ac03f5b285b847cd053abe32e
yyh0126 <yyh0126@naver.com>
2023-04-25 16:03:50 +0900

    Feat : 신고 엔티티 추가

--- backend/nanumi/src/main/java/com/ssafy/nanumi/api/controller/AdminController.java
+++ backend/nanumi/src/main/java/com/ssafy/nanumi/api/controller/AdminController.java
@@ -7,6 +7,7 @@ import com.ssafy.nanumi.config.response.CustomResponse;
 import com.ssafy.nanumi.config.response.ResponseService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +27,15 @@ public class AdminController {
         // TODO : 이렇게 간단하게 해도 되나...
 
         adminService.adminLogin(adminLoginDTO.getEmail(), adminLoginDTO.getPassword());
+
+        return responseService.getSuccessResponse();
+    }
+
+    /* 신고 기록 조회 */
+    @GetMapping("/admin")
+    public CustomResponse findReportList() {
+
+
 
         return responseService.getSuccessResponse();
     }
---
+++ backend/nanumi/src/main/java/com/ssafy/nanumi/db/entity/Report.java
@@ -0,0 +1,47 @@
+package com.ssafy.nanumi.db.entity;
+
+import com.ssafy.nanumi.config.entity.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Report extends BaseTimeEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private long id;
+
+    @Column(name = "content", columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @Column(name = "status", columnDefinition = "TINYINT", nullable = false)
+    private boolean status;
+
+    @Column(name = "stop_date", columnDefinition = "INT", nullable = false)
+    private int stopDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reporter_id")
+    private User reporterId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reported_id")
+    private User reportedId;
+
+    @Builder
+    public Report(long id, String content, boolean status, int stopDate, User reporterId, User reportedId) {
+        this.id = id;
+        this.content = content;
+        this.status = status;
+        this.stopDate = stopDate;
+        this.reporterId = reporterId;
+        this.reportedId = reportedId;
+    }
+}
+
