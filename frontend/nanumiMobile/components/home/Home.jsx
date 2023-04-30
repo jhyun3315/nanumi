9fd11875af10655c3eee8f495e9829ad39758ad2
hsjun1996 <hsjun1996@naver.com>
2023-04-30 19:31:57 +0900

    Feat : get product react-query

--- frontend/nanumiMobile/components/home/Home.jsx
+++ frontend/nanumiMobile/components/home/Home.jsx
@@ -9,17 +9,15 @@ import {productState} from '../../state/product';
 import FocusedStatusBar from '../../ui/FocusedStatusBar';
 import ProductList from '../product/ProductList';
 import {requestGetAllProduct} from '../../api/product';
+import {useQuery} from '@tanstack/react-query';
 
 const Home = () => {
   const navigation = useNavigation();
-  const data = useRecoilValue(productState);
+  // const data = useRecoilValue(productState);
 
-  useEffect(() => {
-    const test = () => {
-      const response = requestGetAllProduct();
-      console.log(response);
-    };
-  }, []);
+  const {data, isLoading, error} = useQuery('allProduct', requestGetAllProduct);
+
+  console.log(data);
   return (
     <SafeAreaView style={styles.container}>
       <FocusedStatusBar background={COLORS.secondary} />
