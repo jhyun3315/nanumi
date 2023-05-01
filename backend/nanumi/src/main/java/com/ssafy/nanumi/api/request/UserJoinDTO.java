c49ccc895049c9eae60ff69f3fdbd46a82554409
이지현 <jiannalee3@gmail.com>
2023-05-01 13:33:49 +0900

    Fix : 회원가입 api 수정

--- backend/nanumi/src/main/java/com/ssafy/nanumi/api/request/UserJoinDTO.java
+++ backend/nanumi/src/main/java/com/ssafy/nanumi/api/request/UserJoinDTO.java
@@ -10,13 +10,13 @@ public class UserJoinDTO {
     private String email;
     private String nickname;
     private String password;
-    private long address_id;
+    private long addressId;
 
     @Builder
-    public UserJoinDTO(String email, String nickname, String password, long address_id) {
+    public UserJoinDTO(String email, String nickname, String password, long addressId) {
         this.email = email;
         this.nickname = nickname;
         this.password = password;
-        this.address_id = address_id;
+        this.addressId = addressId;
     }
 }
