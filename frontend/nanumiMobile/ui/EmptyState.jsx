adae88030708e25fda0d457648e3362529bac697
한상준 <hsjun1996@naver.com>
2023-05-03 17:10:57 +0900

    Design : Empty State

--- frontend/nanumiMobile/ui/EmptyState.jsx
+++ frontend/nanumiMobile/ui/EmptyState.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Dimensions} from 'react-native';
 import {COLORS, FONTS} from '../constants';
+
+const {width, height} = Dimensions.get('window');
 
 const EmptyState = () => {
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+    <View
+      style={{
+        flex: 1,
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: [{translateX: -0.15 * width}, {translateY: 0.01 * height}],
+      }}>
       <Text
         style={{
           fontFamily: FONTS.bold,
