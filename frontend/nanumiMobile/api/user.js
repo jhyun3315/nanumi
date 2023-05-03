ee5a316e24f4e36912534cb312fa874653163cfc
한상준 <hsjun1996@naver.com>
2023-05-03 13:24:56 +0900

    Feat : 회원정보 수정 API

--- frontend/nanumiMobile/api/user.js
+++ frontend/nanumiMobile/api/user.js
@@ -25,3 +25,8 @@ export const requestGetProfile = async userId => {
   const response = await axios.get(`${API_END_POINT}/users/${userId}`);
   return response.data;
 };
+
+export const requestProfileUpdate = async (userId, data) => {
+  const response = await axios.patch(`${API_END_POINT}/users/${userId}`, data);
+  return response.data;
+};
