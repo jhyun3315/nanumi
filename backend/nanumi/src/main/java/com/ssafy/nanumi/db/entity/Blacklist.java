094b74d5ff53c4a67b7cb0762b5271e4f9c4a149
yyh0126 <yyh0126@naver.com>
2023-05-04 17:16:27 +0900

    Feat : Blacklist Entity Mapping

---
+++ backend/nanumi/src/main/java/com/ssafy/nanumi/db/entity/Blacklist.java
@@ -0,0 +1,41 @@
+package com.ssafy.nanumi.db.entity;
+
+import com.ssafy.nanumi.config.entity.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@DynamicInsert
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "blacklists")
+public class Blacklist extends BaseTimeEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private long id;
+
+    @Column(name = "is_blocked", columnDefinition = "TINYINT", nullable = false)
+    private boolean isBlocked;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "blocker_id")
+    private User blocker;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "target_id")
+    private User target;
+
+    @Builder
+    public Blacklist(long id, boolean isBlocked, User blocker, User target) {
+        this.id = id;
+        this.isBlocked = isBlocked;
+        this.blocker = blocker;
+        this.target = target;
+    }
+}
