0d53c01d843561c232fed18fa57d91831b96a72c
한상준 <hsjun1996@naver.com>
2023-05-07 01:29:27 +0900

    Feat : 관리자 로그인

--- frontend/nanumiWeb/src/pages/AdminPage.tsx
+++ frontend/nanumiWeb/src/pages/AdminPage.tsx
@@ -1,8 +1,15 @@
 import SideBar from '../components/sidebar';
 import ContentLeft from '../components/content/ContentLeft';
 import ContentRight from '../components/content/ContentRight';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const AdminPage = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const isAuthenticated = localStorage.getItem('isAuthenticated');
+    if (!isAuthenticated) navigate('/admin/login');
+  }, []);
   return (
     <div className="w-full min-h-screen bg-white flex flex-row">
       <SideBar />
--- frontend/nanumiWeb/src/pages/LoginPage.tsx
+++ frontend/nanumiWeb/src/pages/LoginPage.tsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const LoginPage = () => {
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({ id: '', password: '' });
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleLoginSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (formData.id === 'admin' && formData.password === '1111') {
+      localStorage.setItem('isAuthenticated', 'true');
+      setFormData({ id: '', password: '' });
+      navigate('/admin');
+    } else {
+      alert('아이디나 비밀번호가 일치하지 않습니다.');
+    }
+  };
+
   return (
     <div className="min-h-screen bg-gray-200 flex items-center justify-center">
       <div className="bg-white p-8 rounded shadow-md w-full max-w-md">
         <h2 className="text-2xl font-semibold mb-6">관리자 로그인</h2>
-        <form>
+        <form onSubmit={handleLoginSubmit}>
           <div className="mb-4">
             <label
               className="block text-gray-700 font-bold mb-2"
@@ -18,6 +38,9 @@ const LoginPage = () => {
               id="id"
               type="text"
               placeholder="아이디"
+              name="id"
+              value={formData.id}
+              onChange={handleInputChange}
             />
           </div>
           <div className="mb-6">
@@ -32,12 +55,15 @@ const LoginPage = () => {
               id="비밀번호"
               type="password"
               placeholder="비밀번호"
+              name="password"
+              value={formData.password}
+              onChange={handleInputChange}
             />
           </div>
           <div className="flex items-center justify-between">
             <button
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="button"
+              type="submit"
             >
               로그인
             </button>
