3045b2addc9be8872999a50013661b110a64382d
hsjun1996 <hsjun1996@naver.com>
2023-05-09 05:09:19 +0900

    Feat : FCM

--- frontend/nanumiMobile/App.jsx
+++ frontend/nanumiMobile/App.jsx
@@ -4,9 +4,16 @@ import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import StackNavigator from './navigator/StackNavigator';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import * as encoding from 'text-encoding';
-
+import {
+  requestUserPermission,
+  NotificationListener,
+} from './util/pushnotification_helper';
 const queryClient = new QueryClient();
 const App = () => {
+  useEffect(() => {
+    requestUserPermission();
+    NotificationListener();
+  }, []);
   // useEffect(() => {
   //   const isUser = async () => {
   //     const asyncUser = await AsyncStorage.getItem('user');
