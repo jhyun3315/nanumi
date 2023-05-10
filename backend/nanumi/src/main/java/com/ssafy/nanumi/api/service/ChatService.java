0faf6244bd0cd1d01a7e9de632343795995971fd
김우진 <k0109974@naver.com>
2023-05-10 16:59:05 +0900

    FIX MErege2

--- backend/nanumi/src/main/java/com/ssafy/nanumi/api/service/ChatService.java
+++ backend/nanumi/src/main/java/com/ssafy/nanumi/api/service/ChatService.java
@@ -17,39 +17,50 @@ import com.ssafy.nanumi.db.repository.ChatRoomRepository;
 import com.ssafy.nanumi.db.repository.ProductRepository;
 import com.ssafy.nanumi.db.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 @Service
-@RequiredArgsConstructor
 public class ChatService {
+    // 웹 소켓 메시지를 전송하는데 사용되는 인터페이스
     // 웹 소켓 메시지를 전송하는데 사용되는 인터페이스
     private final SimpMessageSendingOperations messageTemplate;
     private final ChatRepository chatRepository;
     private final ProductRepository productRepository;
     private final ResponseService responseService;
     private final UserRepository userRepository;
-    private final ChatRoomEntity chatRoomEntity;
     private final ChatRoomRepository chatRoomRepository;
+
+    public ChatService(SimpMessageSendingOperations messageTemplate, ChatRepository chatRepository, ProductRepository productRepository, ResponseService responseService, UserRepository userRepository, ChatRoomRepository chatRoomRepository) {
+        this.messageTemplate = messageTemplate;
+        this.chatRepository = chatRepository;
+        this.productRepository = productRepository;
+        this.responseService = responseService;
+        this.userRepository = userRepository;
+        this.chatRoomRepository = chatRoomRepository;
+    }
 
 
     // TODO DTO 객체를 입력으로 받아서, 채팅 메시지를 저장하고 해당 채팅방에 전송한다.
     @Transactional
     public void CreateChat(ChatMessageDTO DTO) {
-
         long user = DTO.getSender();
 
+        // 해당 사용자가 참여 중인 채팅방 목록을 가져옵니다.
         List<ChatRoomEntity> chatRoomEntities = chatRoomRepository.findAllByUserListContaining(user);
-        List<ChatRoomInfoDTO> chatRoomInfoDTOs = new ArrayList<>();
 
+        // DTO의 roomId에 해당하는 채팅방을 찾습니다.
+        ChatRoomEntity chatRoomEntity = chatRoomEntities.stream()
+                .filter(chatRoom -> chatRoom.getChatroomSeq() == DTO.getRoomId()) // 'getRoomId()' 대신 'getChatroomSeq()'를 사용합니다.
+                .findFirst()
+                .orElseThrow(() -> new CustomException(CustomExceptionStatus.NOT_FOUND_CHAT_ROOM));
 
         long opponentId = Arrays.stream(chatRoomEntity.getUserList())
                 .filter(id -> id != user)
@@ -64,7 +75,7 @@ public class ChatService {
                 .orElseThrow(() -> new CustomException(CustomExceptionStatus.NOT_FOUND_USER));
         String profileUrl = senderUser.getProfileUrl();
 
-        // 날짜 형식 및 포멧터를 직접 설정 
+        // 날짜 형식 및 포멧터를 직접 설정
         String pattern = "yyyy-MM-dd a KK:mm ss:SSS";
         DateFormat df = new SimpleDateFormat(pattern);
 
@@ -80,7 +91,6 @@ public class ChatService {
                 .build();
         chatRepository.save(chatEntity);
 
-        // messageTemplate.convertAndSend("/sub/chat/room/" + DTO.getRoomId(), chatEntity);
         // 채팅방의 구독자들에게 채팅 메시지를 전송한다.
         ObjectMapper objectMapper = new ObjectMapper();
         String chatEntityJson = null;
@@ -92,8 +102,9 @@ public class ChatService {
         }
 
         messageTemplate.convertAndSend("/sub/chat/room/" + DTO.getRoomId(), chatEntityJson);
-
     }
+
+
 
 
     // TODO roomSeq를 입력으로 받아서 해당 채팅방의 최근 20개의 채팅 로그를 반환한다.
