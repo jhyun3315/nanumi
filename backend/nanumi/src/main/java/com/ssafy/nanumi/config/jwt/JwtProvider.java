96b575b52b39395230f35b0e0c964f05ef70eea3
김우진 <k0109974@naver.com>
2023-05-10 23:59:04 +0900

    Feat : Token validation refac

--- backend/nanumi/src/main/java/com/ssafy/nanumi/config/jwt/JwtProvider.java
+++ backend/nanumi/src/main/java/com/ssafy/nanumi/config/jwt/JwtProvider.java
@@ -42,8 +42,9 @@ public class JwtProvider {
     private Key secretKey;
 
     // 만료시간 : 1Hour
-    private final long exp = 1000L * 10; // 10초
-    private final long rt_exp = 1000L * 30; // 30초
+    private final long exp = 1000L * 60 * 60 * 24 * 3; // 3일
+    private final long rt_exp = 1000L * 60 * 60 * 24 * 30; // 30일
+
 
     private final JpaUserDetailsService userDetailsService;
     private final UserRepository userRepository;
