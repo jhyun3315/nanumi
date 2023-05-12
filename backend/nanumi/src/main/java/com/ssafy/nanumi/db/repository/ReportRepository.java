adf2092f8d3cd4cd42b0b322459d39920caa3b62
yyh0126 <yyh0126@naver.com>
2023-05-12 15:16:44 +0900

    Fix : Query Param 수정

--- backend/nanumi/src/main/java/com/ssafy/nanumi/db/repository/ReportRepository.java
+++ backend/nanumi/src/main/java/com/ssafy/nanumi/db/repository/ReportRepository.java
@@ -1,6 +1,7 @@
 package com.ssafy.nanumi.db.repository;
 
 import com.ssafy.nanumi.db.entity.Report;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -11,5 +12,5 @@ public interface ReportRepository extends JpaRepository<Report, Long> {
     @Query(value = "select r " +
             "from Report r " +
             "where r.reported.id = :reportedId and r.reporter.id = :reporterId")
-    Optional<Report> findByReportedIdAndReporterId(long reportedId, long reporterId);
+    Optional<Report> findByReportedIdAndReporterId(@Param("reportedId") long reportedId, @Param("reporterId") long reporterId);
 }
