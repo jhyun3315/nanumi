5b8509113ba93195da95ff41e2992fdafeba7292
caerinso <so992419@naver.com>
2023-05-12 10:10:57 +0900

    Fix : review id 추가

--- backend/nanumi/src/main/java/com/ssafy/nanumi/api/response/ReviewReadDTO.java
+++ backend/nanumi/src/main/java/com/ssafy/nanumi/api/response/ReviewReadDTO.java
@@ -5,12 +5,14 @@ import lombok.Getter;
 
 @Getter
 public class ReviewReadDTO {
+    private final long id;
     private final String content;
     private final int starPoint;
     private final int rating;
     private final String writerNickname;
     private final String writerProfileUrl;
     public ReviewReadDTO(Review review){
+        id = review.getId();
         content = review.getContent();
         starPoint = review.getStarPoint();
         rating = review.getRating();
