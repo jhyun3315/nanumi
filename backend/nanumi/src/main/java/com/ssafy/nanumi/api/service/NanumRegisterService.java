d5776c09fc4c7a8400b706b058d1cf6143424ae9
caerinso <so992419@naver.com>
2023-05-12 15:00:29 +0900

    Fix : 나눔 중복처리 코드 추가

--- backend/nanumi/src/main/java/com/ssafy/nanumi/api/service/NanumRegisterService.java
+++ backend/nanumi/src/main/java/com/ssafy/nanumi/api/service/NanumRegisterService.java
@@ -10,6 +10,8 @@ import com.ssafy.nanumi.db.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 @RequiredArgsConstructor
 public class NanumRegisterService {
@@ -20,29 +22,39 @@ public class NanumRegisterService {
     public MatchSuccessDto register(final String key, Long productId, User user) {
         Product product = productRepository.findById(productId)
                 .orElseThrow();
-        int CountMatches = product.getMatches().size();
-        if (CountMatches < 3) {
-            Match match = Match.builder()
-                    .isMatching(false)
-                    .product(product)
-                    .user(user)
-                    .build();
-            Match newMatch = matchRepository.save(match);
-            if (CountMatches == 2) {
-                product.setClosed(true);
-            }
-            return MatchSuccessDto.builder()
-                    .result(true)
-                    .resultMessage("신청 되었습니다.")
-                    .matchId(newMatch.getId())
-                    .build();
-        } else {
-            product.setClosed(true);
+        boolean isDuplicate = matchRepository.existsByProductIdAndUserId(productId, user.getId());
+        if (isDuplicate) {
             return MatchSuccessDto.builder()
                     .result(false)
-                    .resultMessage("인원이 다 찼습니다.")
+                    .resultMessage("중복된 요청 입니다.")
                     .matchId(null)
                     .build();
+        }
+        else {
+            int CountMatches = product.getMatches().size();
+            if (CountMatches < 3) {
+                Match match = Match.builder()
+                        .isMatching(false)
+                        .product(product)
+                        .user(user)
+                        .build();
+                Match newMatch = matchRepository.save(match);
+                if (CountMatches == 2) {
+                    product.setClosed(true);
+                }
+                return MatchSuccessDto.builder()
+                        .result(true)
+                        .resultMessage("신청 되었습니다.")
+                        .matchId(newMatch.getId())
+                        .build();
+            } else {
+                product.setClosed(true);
+                return MatchSuccessDto.builder()
+                        .result(false)
+                        .resultMessage("인원이 다 찼습니다.")
+                        .matchId(null)
+                        .build();
+            }
         }
     }
 
