9593f781e822654ae3ef6a25e8b2f2fe7474d32e
한상준 <hsjun1996@naver.com>
2023-05-14 18:26:29 +0900

    Feat : 메인화면에서 뒤로가기 2번 클릭시 앱 종료

--- frontend/nanumiMobile/screens/HomeScreen.jsx
+++ frontend/nanumiMobile/screens/HomeScreen.jsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Home from '../components/home/Home';
+import {BackHandler, ToastAndroid} from 'react-native';
 
 const HomeScreen = ({navigation}) => {
+  const [exitApp, setExitApp] = useState(false);
+
+  const handleBackPress = () => {
+    if (exitApp) {
+      BackHandler.exitApp();
+    } else {
+      ToastAndroid.show('한 번 더 누르면 종료됩니다', ToastAndroid.SHORT);
+      setExitApp(true);
+
+      setTimeout(() => {
+        setExitApp(false);
+      }, 2000);
+    }
+    return true;
+  };
+
+  useEffect(() => {
+    const backHandler = BackHandler.addEventListener(
+      'hardwareBackPress',
+      handleBackPress,
+    );
+
+    return () => backHandler.remove();
+  }, [exitApp]);
+
   return <Home navigation={navigation} />;
 };
 
