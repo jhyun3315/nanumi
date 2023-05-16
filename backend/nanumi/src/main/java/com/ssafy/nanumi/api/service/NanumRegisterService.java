0e543b9b50797565e01795580d3f98dd0f93686d
caerinso <so992419@naver.com>
2023-05-16 11:52:09 +0900

    Test : 12:00-12:05

--- backend/nanumi/src/main/java/com/ssafy/nanumi/api/service/NanumRegisterService.java
+++ backend/nanumi/src/main/java/com/ssafy/nanumi/api/service/NanumRegisterService.java
@@ -30,7 +30,7 @@ public class NanumRegisterService {
         // 서버시간 확인.
         LocalTime currentTime = LocalTime.now();
         // 오후 2시에서 3시 사이 확인
-        if (!(currentTime.isAfter(LocalTime.of(10, 25)) & currentTime.isBefore(LocalTime.of(10, 35)))) {
+        if (!(currentTime.isAfter(LocalTime.of(12, 0)) & currentTime.isBefore(LocalTime.of(12, 5)))) {
             return MatchSuccessDto.builder()
                     .result(false)
                     .resultMessage("상품 나눔 시간이 아닙니다.")
